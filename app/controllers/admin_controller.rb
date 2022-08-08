101d41e301b581993aa11dd6eab68a2951e6e440
arhumsharif <arhumsharif06@gmail.com>
2022-08-08 16:30:50 +0500

    Admin Dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,7 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   def dashboard
   end
+  def AddRestaurant
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :admins
   get 'admin/dashboard' , to: 'admin#dashboard'
+  get 'admin/addrestaurant', to: 'admin#AddRestaurant'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
