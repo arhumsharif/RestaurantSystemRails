b60bf6878a5a3a19e63940ae1d6c135abc67137b
arhumsharif <arhumsharif06@gmail.com>
2022-08-14 22:22:05 +0500

    Customer  Model has been generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_14_171711) do
+ActiveRecord::Schema.define(version: 2022_08_14_172100) do
 
   create_table "addons", force: :cascade do |t|
     t.string "addon_name"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2022_08_14_171711) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_categories_on_item_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "deal_items", force: :cascade do |t|
